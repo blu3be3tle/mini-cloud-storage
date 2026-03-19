4b17006d0b7eae2a8a7d12f7abc23c877a093f9d
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2026-03-19 06:42:35 +0600

    feat(fileRoutes): add routes for files and storage crud

--- src/routes/fileRoutes.js
+++ src/routes/fileRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express")
 const router = express.Router()
 
-// test route
-router.get("/", (req, res) => {
-    res.send("File routes working")
-})
+const fileController = require("../controllers/fileController")
+
+router.post("/:user_id/files", fileController.uploadFile)
+
+router.delete("/:user_id/files/:file_id", fileController.deleteFile)
+
+router.get("/:user_id/storage-summary", fileController.getStorageSummary)
+
+router.get("/:user_id/files", fileController.listFiles)
 
 module.exports = router
